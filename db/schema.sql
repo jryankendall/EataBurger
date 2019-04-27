6657cde15981f239c8d2a4837297893755f98dd1
unknown <jed@jedkendall.com>
2019-04-27 18:38:44 -0500

    Final deployment and security issues fixed. Should be fully functional now. Finished!

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,10 @@ CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INTEGER(3) AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(60) NOT NULL,
     devoured BOOLEAN NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
 
-    PRIMARY KEY (id)
+    PRIMARY KEY(id)
 );
