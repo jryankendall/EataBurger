2ea049be37362114eb9a53034890260dffdc6a37
unknown <jed@jedkendall.com>
2019-04-27 18:00:26 -0500

    Password fix 1

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: PORT,
     user: "root",
-    password: login.password,
+    password: "heroku_pass",
     database: "burgers_db"
 });
 
